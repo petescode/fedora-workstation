431d0c35337794400517d5819af18fe63e53abbc
petescode <45579592+petescode@users.noreply.github.com>
2023-05-20 16:28:30 -0400

    finish setup.py and remove setup.sh

--- setup.py
+++ setup.py
@@ -22,11 +22,15 @@ if args.git_user and args.git_email is None:
 if args.git_email and args.git_user is None:
     parser.error("-e or --git-email requires -u or --git-user")
 
+# Make sure Ansible is installed before trying to execute a playbook
 print("\nInstalling Ansible...\n")
 subprocess.run(["sudo", "dnf", "install", "ansible", "-y"])
 
+# Pass appropriate option variables to Ansible playbook
 if args.git_user is not None and args.hostname is not None:
-    print("execute playbook with all extra vars")
+    #print("execute playbook with all extra vars")
+    print(f'\nCommand being executed is:\nansible-playbook {yaml_path} --extra-vars "git_user={args.git_user} git_email={args.git_email} system_hostname={args.hostname}" --ask-become-pass\n')
+    subprocess.run(["ansible-playbook", f"{yaml_path}", "--extra-vars", f"git_user={args.git_user} git_email={args.git_email} system_hostname={args.hostname}", "--ask-become-pass"])
 
 elif args.git_user is not None and args.hostname is None:
     #print("execute playbook only with git extra vars")
@@ -41,5 +45,4 @@ elif args.git_user is None and args.hostname is not None:
 else:
     #print("execute playbook only with no extra vars (default)")
     print(f"\nCommand being executed is:\nansible-playbook {yaml_path} --ask-become-pass\n")
-    
     subprocess.run(["ansible-playbook", f"{yaml_path}", "--ask-become-pass"])
