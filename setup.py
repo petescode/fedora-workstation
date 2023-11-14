dec2f95296d470932e7edd5be733e64ee2e645e8
petescode <45579592+petescode@users.noreply.github.com>
2023-11-14 07:30:06 -0500

    initial branch change to v39

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import argparse
 import subprocess
 from pathlib import Path
 
-yaml_path = str(Path.cwd()) + "/fedora38-workstation.yml"
+yaml_path = str(Path.cwd()) + "/fedora39-workstation.yml"
 
 # Initialize parser
 parser = argparse.ArgumentParser()
